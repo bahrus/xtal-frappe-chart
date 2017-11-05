a4472de7d6353fdcc339ddd7766b568d792cf29e
bahrus <anderson.bruce.b@gmail.com>
2017-11-05 08:47:10 -0500

    frappe charts

--- xtal-frappe-chart.js
+++ xtal-frappe-chart.js
@@ -1,5 +1,7 @@
+;
 (function () {
     const _lpn = 'lib-path';
+    const _dn = 'data';
     class XtalFrappeChart extends HTMLElement {
         constructor() {
             super(...arguments);
@@ -11,6 +13,13 @@
         set libPath(val) {
             this.setAttribute(_lpn, val);
         }
+        get data() {
+            return this._data;
+        }
+        set data(val) {
+            this._data = val;
+            this.connectedCallback();
+        }
         static get observedAttributes() {
             return [
                 /** @type {string} Url to Frappe Library. By default uses
@@ -18,6 +27,10 @@
                  *  but you can install a local copy and use this property to point to it.
                  */
                 _lpn,
+                /**
+                 * @type {object} Data to chart
+                 */
+                _dn,
             ];
         }
         attributeChangedCallback(name, oldValue, newValue) {
@@ -25,8 +38,57 @@
                 case _lpn:
                     this._libPath = newValue;
                     break;
+                case _dn:
+                    this._data = JSON.parse(newValue);
+                    break;
             }
         }
+        loadChart() {
+            if (!this._data)
+                return;
+            this._chart = new Chart(this._data);
+        }
+        connectedCallback() {
+            if (!Chart) {
+                this.downloadJSFilesInParallelButLoadInSequence([
+                    {
+                        src: this._libPath
+                    }
+                ]).then(() => this.loadChart());
+            }
+            else {
+                this.loadChart();
+            }
+        }
+        downloadJSFilesInParallelButLoadInSequence(refs) {
+            //see https://www.html5rocks.com/en/tutorials/speed/script-loading/
+            return new Promise((resolve, reject) => {
+                const notLoadedYet = {};
+                const nonNullRefs = refs.filter(ref => ref !== null);
+                nonNullRefs.forEach(ref => {
+                    notLoadedYet[ref.src] = true;
+                });
+                nonNullRefs.forEach(ref => {
+                    const script = document.createElement('script');
+                    script.src = ref.src;
+                    script.async = false;
+                    script.onload = () => {
+                        //delete notLoadedYet[script.src];
+                        Object.keys(notLoadedYet).forEach(key => {
+                            if (script.src.endsWith(key)) {
+                                delete notLoadedYet[key];
+                                return;
+                            }
+                        });
+                        if (Object.keys(notLoadedYet).length === 0) {
+                            resolve();
+                        }
+                    };
+                    document.head.appendChild(script);
+                });
+            });
+        }
     }
+    customElements.define('xtal-frappe-chart', XtalFrappeChart);
 })();
 //# sourceMappingURL=xtal-frappe-chart.js.map
--- xtal-frappe-chart.ts
+++ xtal-frappe-chart.ts
@@ -1,13 +1,26 @@
+declare class Chart{constructor(data)};
 (function () {
+    interface IDynamicJSLoadStep{
+        src?: string;
+    }
     const _lpn = 'lib-path';
+    const _dn = 'data';
     class XtalFrappeChart extends HTMLElement{
         _libPath = 'https://unpkg.com/frappe-charts@0.0.3/dist/frappe-charts.min.iife.js';
-        
+        _data: object;
+        _chart: Chart;
         get libPath(){
             return this._libPath;
         }
         set libPath(val){
             this.setAttribute(_lpn, val)
+        }
+        get data(){
+            return this._data;
+        }
+        set data(val){
+            this._data = val;
+            this.connectedCallback();
         }
         static get observedAttributes() {
             return [
@@ -16,6 +29,10 @@
                  *  but you can install a local copy and use this property to point to it.
                  */
                 _lpn,
+                /**
+                 * @type {object} Data to chart
+                 */
+                _dn,
             ]
         }
         attributeChangedCallback(name: string, oldValue: string, newValue: string) {
@@ -23,7 +40,56 @@
                 case _lpn:
                     this._libPath = newValue;
                     break;
+                case _dn:
+                    this._data = JSON.parse(newValue);
+                    break;
             }
         }
+        loadChart(){
+            if(!this._data) return;
+            this._chart = new Chart(this._data);
+        }
+
+        connectedCallback(){
+            if(!Chart){
+                this.downloadJSFilesInParallelButLoadInSequence([
+                    {
+                        src: this._libPath
+                    }
+                ]).then(() => this.loadChart())
+            }else{
+                this.loadChart();
+            }
+        }
+        downloadJSFilesInParallelButLoadInSequence(refs: IDynamicJSLoadStep[]){
+            //see https://www.html5rocks.com/en/tutorials/speed/script-loading/
+            return new Promise((resolve, reject) => {
+                const notLoadedYet : {[key: string] : boolean} = {};
+                const nonNullRefs = refs.filter(ref => ref !== null);
+                nonNullRefs.forEach(ref => {
+                    notLoadedYet[ref.src] = true;
+                });
+                nonNullRefs.forEach(ref =>{
+                    const script = document.createElement('script');
+                    script.src = ref.src;
+                    script.async = false;
+                    script.onload = () =>{
+                        //delete notLoadedYet[script.src];
+                        Object.keys(notLoadedYet).forEach(key =>{
+                            if(script.src.endsWith(key)){
+                                delete notLoadedYet[key];
+                                return;
+                            }
+                        })
+                        if(Object.keys(notLoadedYet).length === 0){
+                            resolve();
+                        }
+                    }
+                    document.head.appendChild(script);
+                });
+            })
+
+        }
     }
+    customElements.define('xtal-frappe-chart', XtalFrappeChart);
 })();
