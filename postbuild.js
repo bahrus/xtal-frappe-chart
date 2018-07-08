e2536e10d86660e2997848eab420df267414583d
bahrus <anderson.bruce.b@gmail.com>
2018-07-08 11:22:17 -0400

    ...

--- postbuild.js
+++ postbuild.js
@@ -1,7 +1,7 @@
 //@ts-check
 const jiife = require('jiife');
 jiife.addFiles(['node_modules/frappe-charts/dist/frappe-charts.min.iife.js']);
-jiife.processFiles(['xtal-frappe-chart.js'], 'xtal-frappe-chart.iife.js');
+jiife.processFiles(['node_modules/xtal-latx/xtal-latx.js','xtal-frappe-chart.js'], 'xtal-frappe-chart.iife.js');
 
 
 
