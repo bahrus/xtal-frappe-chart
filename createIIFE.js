18150c6603a4fb8e6881e471207b63fbf519ba2b
bahrus <anderson.bruce.b@gmail.com>
2019-12-15 06:53:04 -0500

    ...

--- createIIFE.js
+++
@@ -1,9 +0,0 @@
-//@ts-check
-const jiife = require('jiife');
-jiife.addFiles(['node_modules/frappe-charts/dist/frappe-charts.min.iife.js']);
-const xl = 'node_modules/xtal-element/';
-jiife.processFiles([xl + 'xtal-latx.js', xl + 'define.js', 'xtal-frappe-chart.js'], 'dist/xtal-frappe-chart.iife.js');
-
-
-
-
