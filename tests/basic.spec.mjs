a0ad125802ef180f6e85f644e7d5cad709f7831b
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-02-09 14:32:20 -0500

    0.0.107

--- tests/basic.spec.mjs
+++ tests/basic.spec.mjs
@@ -4,6 +4,6 @@ test('basic', async ({ page }) => {
   await page.goto('./tests/basic.html');
   // wait for 1 second
   await page.waitForTimeout(4000);
-  const editor = page.locator('#target');
+  const editor = page.locator('#target2');
   await expect(editor).toHaveAttribute('mark', 'good');
 });
